512c0c794e3aca8e743e1af8eed879cff15337cb
azerbelhedi <azerbelhedide@gmail.com>
2018-12-21 11:57:30 +0100

    ctr c sec && select design

--- mchbchtal9ah/l9itous.js
+++ mchbchtal9ah/l9itous.js
@@ -1,7 +1,10 @@
 document.addEventListener("keydown" , (e) => {
 	//console.log(e.which) ;
+	//alert(e.which) ;
 	if(e.which === 123){e.preventDefault();}
+	if(e.which === 67){e.preventDefault();}
 });
+
 
 console.log("hey") ;
 var globalUser ;
